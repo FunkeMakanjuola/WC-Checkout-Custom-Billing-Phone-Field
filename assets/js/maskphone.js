fee29a74f02d752c99b23ec3118988c9b225f451
FunkeMakanjuola <40076042+FunkeMakanjuola@users.noreply.github.com>
2018-06-13 18:17:55 -0400

    update maskphone.js

--- assets/js/maskphone.js
+++ assets/js/maskphone.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
-    $("#billing_phone").mask("+1(999) 999-9999");
+jQuery(document).ready(function () {
+    jQuery("#billing_phone").mask("+1(999) 999-9999");
 
 });
